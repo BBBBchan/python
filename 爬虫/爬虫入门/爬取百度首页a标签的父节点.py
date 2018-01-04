7a920778aa96342dc4f55eb2256e2941e68da4c9
BBchan <sbysbysby123@gmail.com>
2018-01-04 17:15:10 +0800

    12

--- 爬虫/爬虫入门/爬取百度首页a标签的父节点.py
+++ 爬虫/爬虫入门/爬取百度首页a标签的父节点.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 try:
 	url = 'http://www.baidu.com'
-	r = requests.get(url,timeout = 5)
+	r = requests.get(url, timeout=5)
 	r.raise_for_status()
 	r.encoding = r.apparent_encoding
 	temp = r.text
