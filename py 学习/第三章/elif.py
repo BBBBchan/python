a25fc1f242bffaa828d80be9a0b1439ecac773c2
BBchan <sbysbysby123@gmail.com>
2018-02-10 15:00:13 +0800

    1

---
+++ py 学习/第三章/elif.py
@@ -0,0 +1,9 @@
+color = "puce"
+if color == "red":
+	print("It's a tomato.")
+elif color == "green":
+	print("It's a green pepper.")
+elif color == "bee purple":
+	print("I don't konw what it is, but only bees can see it.")
+else:
+	print("I've never heard of the color", color)
---
+++ py 学习/第三章/if.py
@@ -0,0 +1,12 @@
+furry = True
+small = True
+if furry:
+	if small:
+		print("It is a cat.")
+	else:
+		print("It's a bear!")
+else:
+	if small:
+		print("It's a skink!")
+	else:
+		print("It's a human. Or a hairless bear.")
